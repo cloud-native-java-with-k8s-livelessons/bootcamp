4f58df9551378ebcf9fb63380c22476179bd8a6c
Josh Long <josh@joshlong.com>
2021-09-12 20:22:49 -0700

    deploy

---
+++ reactive/src/test/java/com/example/basicsreactive/BasicsReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicsreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicsReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
