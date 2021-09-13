97bc18ac4f25c1ea23f9d4b00899678522200e0b
Josh Long <josh@joshlong.com>
2021-09-12 19:38:24 -0700

    spring boot

---
+++ spring-boot/src/main/java/com/example/spring/Application.java
@@ -0,0 +1,66 @@
+package com.example.spring;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.util.Objects;
+
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	ApplicationRunner runner( CustomerService customerService) {
+		return args -> System.out.println(customerService.save("Tammie"));
+	}
+
+}
+
+interface CustomerService {
+	Customer save(String name);
+
+	Customer findById(Integer id);
+}
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+class JdbcCustomerService implements CustomerService {
+
+	private final JdbcTemplate template;
+
+	@Override
+	public Customer save(String name) {
+
+		var gkh = new GeneratedKeyHolder();
+		this.template.update(connection -> {
+			var ps = connection
+				.prepareStatement("insert into customer(name) values(?)", PreparedStatement.RETURN_GENERATED_KEYS);
+			ps.setString(1, name);
+			return ps;
+		}, gkh);
+		var id = (Number) (Objects.requireNonNull(gkh.getKeys())).values().iterator().next();
+		return findById(id.intValue());
+	}
+
+	@Override
+	public Customer findById(Integer id) {
+		var q = "select * from customer where id = ? ";
+		return template.queryForObject(q, (rs, i) -> new Customer(rs.getInt("id"), rs.getString("name")), id);
+	}
+}
